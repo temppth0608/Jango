38d1cbd0bbf3077e9eb61280fed6e1f4215931b3
SimHyunJeong <tkfkdgo2828@naver.com>
2015-12-25 15:32:04 +0900

    MainVC 진행중  by hyunjeong.sim

--- jango/MainViewController.swift
+++ jango/MainViewController.swift
@@ -8,11 +8,15 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
+class MainViewController: UIViewController,UITableViewDataSource {
 
     @IBOutlet weak var jangoSayTableView: UITableView!
+    
+    var jangoSay = ["안녕, 나는 장고야! 우리집을 깨끗이 유지해줘!!","11월 16일에 산 우유의 유통기한이 2틀 남았어!!"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -21,7 +25,20 @@ class MainViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    internal func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return 2
+    }
     
+    internal func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        let cell = jangoSayTableView.dequeueReusableCellWithIdentifier("JangoSayTableViewCell", forIndexPath: indexPath) as! JangoSayTableViewCell
+        
+        cell.jangoImageView.image = UIImage(named: "jango.png")
+        cell.ballonImageView.image = UIImage(named: "balloon.png")
+        cell.jangoLabel.text = jangoSay[indexPath.row]
+        
+        return cell
+    }
 
 }
-
