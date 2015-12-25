2d0a99f213cf544f19c879a9814e45036bebd9cd
박태현 <ppth0608@naver.com>
2015-12-25 11:20:28 +0900

    initial commit by taehyun.park

---
+++ jango/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  jango
+//
+//  Created by 박태현 on 2015. 12. 25..
+//  Copyright © 2015년 hyunjeong,sanghyun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
